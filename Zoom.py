6935a3a454aef536becab29f40644ec6efa560d7
Mizan-Ali <64581554+Mizan-Ali@users.noreply.github.com>
2020-08-26 08:54:18 +0530

    Update Zoom.py

--- Zoom.py
+++ Zoom.py
@@ -14,10 +14,12 @@ while(True):
     if timestr in df.Time.values:
         df_new = df[df['Time'].astype(str).str.contains(timestr)]
         #Open the Zoom app
-        '''Example:
-        subprocess.call("C:\\Users\\USER\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")'''
+        #Example:
+        #subprocess.call("C:\\Users\\USER\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+        
         subprocess.call("Enter the file location of Zoom.exe here")
         time.sleep(8)
+        
         #Locate the position of the join button on the screen
         position = pyautogui.locateOnScreen("buttons\\join_button.png")
         #Move the cursor to the position of the button
